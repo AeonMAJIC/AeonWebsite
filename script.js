846825ddc70caee0f0619ff01e1b6bbdd3513077
Florian Boireau <florian23russie@hotmail.fr>
2023-01-26 04:03:32 +0100

    anchos corrected, mirror button done

--- script.js
+++ script.js
@@ -24,11 +24,35 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
 }
 
-//Initialisation au sprélancement
+//Initialisation au prélancement
 window.addEventListener("DOMContentLoaded", function()
 {
+    //Mise a jour de la taille de la barre derriere la barre du haut pour que rien ne soit caché par celle-ci
     let barreTopHeight = document.getElementsByClassName("BarreTop")[0].clientHeight;
     document.getElementsByClassName("DerriereBarreTop")[0].style.height = barreTopHeight + "px";
+
+    //Mise a jour de la taille des boutons pour tous match ensemble
+    const arrayBtn = [document.getElementById("btn1"), document.getElementById("btn2"), document.getElementById("btn3")]
+
+    var widthmax  = 0;
+    var heightmax = 0;
+    for (let index = 0; index < arrayBtn.length; index++)
+    {
+        if(arrayBtn[index].clientWidth > widthmax)
+            widthmax = arrayBtn[index].clientWidth;
+
+        if(arrayBtn[index].clientHeight > heightmax)
+            heightmax = arrayBtn[index].clientHeight;
+    }
+
+    document.getElementById("btn1").style.width = widthmax + "px";
+    document.getElementById("btn2").style.width = widthmax + "px";
+    document.getElementById("btn3").style.width = widthmax + "px";
+
+    document.getElementById("btn1").style.height = heightmax + "px";
+    document.getElementById("btn2").style.height = heightmax + "px";
+    document.getElementById("btn3").style.height = heightmax + "px";
+    
 });
 
 //Initialisation au lancement
