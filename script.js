28a8d80ec57af4b2ffa3adff84774f98bc965159
Florian Boireau <florian23russie@hotmail.fr>
2023-01-28 01:36:36 +0100

    theme diapo have their own slideindex value, they aren't connected now

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //Écrit par Florian Boireau
 
-var aSlideIndex;
+var aSlideIndex = []; //array of index, every diaporama get a case
 
 /**
  * Show the next slide.
  * @param {*} diapositiveNumber The diapositive number to be shown.
  * @param {*} diaporamaNumber The diaporama (Theme) number.
  */
-function plusSlides(diapositiveNumber, diaporamaNumber)
+function plusSlide(diapositiveNumber, diaporamaNumber)
 {
-    showSlides(aSlideIndex += diapositiveNumber, diaporamaNumber);
+    showSlides(aSlideIndex[diaporamaNumber] += diapositiveNumber, diaporamaNumber);
 }
 
 /**
@@ -19,7 +19,7 @@ function plusSlides(diapositiveNumber, diaporamaNumber)
  */
 function currentSlide(diapositiveNumber, diaporamaNumber)
 {
-    showSlides(aSlideIndex = diapositiveNumber, diaporamaNumber);
+    showSlides(aSlideIndex[diaporamaNumber] = diapositiveNumber, diaporamaNumber);
 }
 
 /**
@@ -34,17 +34,17 @@ function showSlides(diapositiveNumber, diaporamaNumber)
     var selectors = document.querySelectorAll('.selector[data-groupe="'      + diaporamaNumber + '"]');
 
     if (diapositiveNumber > slides.length)
-        aSlideIndex = 1
+        aSlideIndex[diaporamaNumber] = 1
     if (diapositiveNumber < 1)
-        aSlideIndex = slides.length
+        aSlideIndex[diaporamaNumber] = slides.length
 
     for (i = 0; i < slides.length; i++)
         slides[i].style.display = "none";
     for (i = 0; i < selectors.length; i++)
         selectors[i].className = selectors[i].className.replace(" active", "");
 
-    slides[aSlideIndex-1].style.display = "block";
-    selectors[aSlideIndex-1].className += " active";
+    slides[aSlideIndex[diaporamaNumber]-1].style.display = "block";
+    selectors[aSlideIndex[diaporamaNumber]-1].className += " active";
 }
 
 /** 
@@ -70,6 +70,8 @@ function firstBar()
  */
 function firstSelectors(diaporamaNumber)
 {
+    aSlideIndex[diaporamaNumber] = 1;
+    console.error(diaporamaNumber);
     var slides    = document.querySelectorAll('.custom-slider[data-groupe="' + diaporamaNumber + '"]');
     var selectors = document.querySelectorAll('.selector[data-groupe="'      + diaporamaNumber + '"]');
 
@@ -78,7 +80,7 @@ function firstSelectors(diaporamaNumber)
         selectors[i].style.backgroundImage = 'url(' + slides[i].querySelector('.slide-img').src + ')';
     }
 
-    showSlides(aSlideIndex, diaporamaNumber);
+    showSlides(aSlideIndex[diaporamaNumber], diaporamaNumber);
 }
 
 /** 
@@ -115,8 +117,8 @@ function firstUpdate()
 {
     firstBar();
 
+    firstSelectors(0);
     firstSelectors(1);
-    firstSelectors(2);
 
     firstDownloadButtons();
 }
